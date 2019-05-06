708a9085d70d9f01b4d6ad3a60bcf3e188cdd8d6
anlongfei <dragonfly2012an@gmail.com>
2019-05-06 19:08:31 -0400

    hello world

---
+++ 01/hello.cpp
@@ -0,0 +1,30 @@
+/* ====================================================
+ * #   Copyright (C)2019 All rights reserved.
+ * #
+ * #   Author        : anlongfei
+ * #   File Name     : cpython.cpp
+ * #   Last Modified : 2019-06-05 14:39
+ * #   Describe      :
+ * #
+ * # ====================================================*/
+
+#include <iostream>
+#include <python3.6m/Python.h>
+using namespace std;
+bool py_init() {
+    Py_Initialize();
+    return Py_IsInitialized();
+
+}
+int main(int argc, char *argv[])
+{
+    if (py_init()) {
+        std::cerr << "python init fail !!!" << std::endl;
+        return -1;
+    }
+    PyRun_SimpleString("print('Hello World!')");
+    Py_Finalize();
+    return 0;
+}
+
+// g++ -lpython3.6m hello.cpp
